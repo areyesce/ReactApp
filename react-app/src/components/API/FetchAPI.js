75c8d96dd35c7ccce1916d429007df4249b525e7
AVR <areyesc1@swarthmore.edu>
2023-02-01 16:51:33 -0700

    updated FetchAPI displays data

--- react-app/src/components/API/FetchAPI.js
+++ react-app/src/components/API/FetchAPI.js
@@ -1,5 +1,4 @@
-import React, { useState} from 'react'
-// import {useEffect} from 'react'
+import React, { useState, useEffect} from 'react'
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
@@ -9,85 +8,87 @@ import ListItemText from '@mui/material/ListItemText';
 // END OF GOOGLE API LINES
 
 
-async function FetchAPI() {
-
+function FetchAPI() {
     const [data, setData] = useState([])
     const url = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValuesExtended/1FTFW1ET7CFC19542?format=json'
-    const apiGet = async () => {
-        const response = await fetch(url)
-        setData(response.json())
-        console.log(response)
-        return await response.json()
-    // file might already be json
-        // await fetch(url)
-        //     .then((response) => response.json())
-        //     .then((json) => {
-        //         console.log(json);
-        //         setData(json);
-        //         // setData({data: data.Results});
-        //     });
-    };
-    // AUTOMATICALY CLICKS FETCH API BUTTON
-    // useEffect(() => {
-    //     apiGet();
-    // }, []);
 
-    return (
-        
-        <div>
-            My API <br />
-            <button onClick={apiGet}>Fetch API</button>
-            <br />
-        
-            <pre>{JSON.stringify(data.Results[0],null,2)}</pre>
-            {/* CONSIDER MAKING THIS A TABLE */}
-            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                <ListItem>
-                    <ListItemText primary="Year" secondary={JSON.stringify(data.Results[0].ModelYear,null,2)}> </ListItemText>
-                </ListItem>
-                <ListItem>
-                    {/* <ListItemText primary="Make" secondary={JSON.stringify(data.Results[0].Make,null,2)}> </ListItemText> */}
-                </ListItem>
-                <ListItem>
-                    {/* <ListItemText primary="Model" secondary={JSON.stringify(data.Results[0].Model,null,2)}> </ListItemText> */}
-                </ListItem>
-                <ListItem>
-                    {/* <ListItemText primary="Body Style" secondary={JSON.stringify(data.Results[0].BodyClass,null,2)}> </ListItemText> */}
-                </ListItem>
-                {/* SIX OTHER VEHICLE FEATURES */}
-                <ListItem>
-                    {/* <ListItemText primary="Vehicle Type" secondary={JSON.stringify(data.Results[0].VehicleType,null,2)}> </ListItemText> */}
-                </ListItem>
-                <ListItem>
-                    {/* <ListItemText primary="Primary Fuel Type" secondary={JSON.stringify(data.Results[0].FuelTypePrimary,null,2)}> </ListItemText> */}
-                </ListItem>
-                <ListItem>
-                    {/* <ListItemText primary="Fuel Injection Type" secondary={JSON.stringify(data.Results[0].FuelInjectionType,null,2)}> </ListItemText> */}
-                </ListItem>
-                <ListItem>
-                    {/* <ListItemText primary="Brake System Type" secondary={JSON.stringify(data.Results[0].BrakeSystemType,null,2)}> </ListItemText> */}
-                </ListItem>
-                <ListItem>
-                    {/* <ListItemText primary="Drive Type" secondary={JSON.stringify(data.Results[0].DriveType,null,2)}> </ListItemText> */}
-                </ListItem>
-                <ListItem>
-                    {/* <ListItemText primary="Maximum Weight Rating" secondary={JSON.stringify(data.Results[0].GVWR,null,2)}> </ListItemText> */}
-                </ListItem>
-                
-                
-            </List> 
+    
+    useEffect( () => {
+      async function fetchInfo(){
+          const response = await fetch(url)
+          const responseJson = await response.json();
+          setData(responseJson.Results);
+      }
+      fetchInfo();
+    }, []);
 
-            {/* <pre>{JSON.stringify(data.Results[0].Make,null,2)}</pre>
-            
-            {/* <div>
-                <ul>
-                    {data.Results.map(item =>
-                        <li>{item}</li>
-                        )}
-                </ul>
-            </div> */}
-        </div>
-    );
+
+    // Info is basically data.Results[0]//
+    // Need to do button function, this just displays the data.
+    // to use button, I think you have to do button onClick={example}>text...</button>
+    
+    const getInfo = data.map((info) => {
+      return(
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <ListItem>
+            <ListItemText primary="Year" secondary={info.ModelYear}> </ListItemText>
+        </ListItem>
+        <ListItem>
+            { <ListItemText primary="Make" secondary={info.Make}> </ListItemText> }
+        </ListItem>
+        <ListItem>
+            {<ListItemText primary="Model" secondary={info.Model}> </ListItemText> }
+        </ListItem>
+        <ListItem>
+            { <ListItemText primary="Body Style" secondary={info.BodyClass}> </ListItemText> }
+        </ListItem>
+        <ListItem>
+            { <ListItemText primary="Vehicle Type" secondary={info.VehicleType}> </ListItemText> }
+        </ListItem>
+        <ListItem>
+            { <ListItemText primary="Primary Fuel Type" secondary={info.FuelTypePrimary}> </ListItemText> }
+        </ListItem>
+        <ListItem>
+            { <ListItemText primary="Fuel Injection Type" secondary={info.FuelInjectionType}> </ListItemText> }
+        </ListItem>
+        <ListItem>
+            {<ListItemText primary="Brake System Type" secondary={info.BrakeSystemType}> </ListItemText> }
+        </ListItem>
+        <ListItem>
+            { <ListItemText primary="Drive Type" secondary={info.DriveType}> </ListItemText> }
+        </ListItem>
+        <ListItem>
+            { <ListItemText primary="Maximum Weight Rating" secondary={info.GVWR}> </ListItemText> }
+        </ListItem>
+    </List> 
+      )
+    })
+
+    return(
+      <>
+      <div>
+        {getInfo}
+        {/* My API <br />
+        <button onClick={getInfo}>Fetch API</button>
+        <br /> */}
+      </div>
+      </>
+      
+
+      // )}
+      // {data.map(info => <div>{info.Make}</div>)}
+      // {data.map(info => <div>{info.Model}</div>)}
+      // {data.map(info => <div>{info.BodyClass}</div>)}
+      // {data.map(info => <div>{info.VehicleType}</div>)}
+      // {data.map(info => <div>{info.FuelTypePrimary}</div>)}
+      // {data.map(info => <div>{info.FuelInjectionType}</div>)}
+      // {data.map(info => <div>{info.BrakeSystemType}</div>)}
+      // {data.map(info => <div>{info.DriveType}</div>)}
+      // {data.map(info => <div>{info.GVWR}</div>)}
+      // </div>
+    )
+
+ 
 }
 
 export default FetchAPI;
