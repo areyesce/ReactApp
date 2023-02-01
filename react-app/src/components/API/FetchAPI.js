c804974e8ce0b0a4ccdb2ef8be1f2e2284aaf4bb
AVR <areyesc1@swarthmore.edu>
2023-02-01 13:34:45 -0700

    update FetchAPI

--- react-app/src/components/API/FetchAPI.js
+++ react-app/src/components/API/FetchAPI.js
@@ -9,19 +9,23 @@ import ListItemText from '@mui/material/ListItemText';
 // END OF GOOGLE API LINES
 
 
-function FetchAPI() {
+async function FetchAPI() {
 
     const [data, setData] = useState([])
     const url = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValuesExtended/1FTFW1ET7CFC19542?format=json'
-    const apiGet = () => {
+    const apiGet = async () => {
+        const response = await fetch(url)
+        setData(response.json())
+        console.log(response)
+        return await response.json()
     // file might already be json
-        fetch(url)
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json);
-                setData(json);
-                // setData({data: data.Results});
-            });
+        // await fetch(url)
+        //     .then((response) => response.json())
+        //     .then((json) => {
+        //         console.log(json);
+        //         setData(json);
+        //         // setData({data: data.Results});
+        //     });
     };
     // AUTOMATICALY CLICKS FETCH API BUTTON
     // useEffect(() => {
@@ -35,39 +39,39 @@ function FetchAPI() {
             <button onClick={apiGet}>Fetch API</button>
             <br />
         
-            {/* <pre>{JSON.stringify(data.Results[0].Make,null,2)}</pre> */}
+            <pre>{JSON.stringify(data.Results[0],null,2)}</pre>
             {/* CONSIDER MAKING THIS A TABLE */}
             <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
                 <ListItem>
                     <ListItemText primary="Year" secondary={JSON.stringify(data.Results[0].ModelYear,null,2)}> </ListItemText>
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Make" secondary={JSON.stringify(data.Results[0].Make,null,2)}> </ListItemText>
+                    {/* <ListItemText primary="Make" secondary={JSON.stringify(data.Results[0].Make,null,2)}> </ListItemText> */}
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Model" secondary={JSON.stringify(data.Results[0].Model,null,2)}> </ListItemText>
+                    {/* <ListItemText primary="Model" secondary={JSON.stringify(data.Results[0].Model,null,2)}> </ListItemText> */}
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Body Style" secondary={JSON.stringify(data.Results[0].BodyClass,null,2)}> </ListItemText>
+                    {/* <ListItemText primary="Body Style" secondary={JSON.stringify(data.Results[0].BodyClass,null,2)}> </ListItemText> */}
                 </ListItem>
                 {/* SIX OTHER VEHICLE FEATURES */}
                 <ListItem>
-                    <ListItemText primary="Vehicle Type" secondary={JSON.stringify(data.Results[0].VehicleType,null,2)}> </ListItemText>
+                    {/* <ListItemText primary="Vehicle Type" secondary={JSON.stringify(data.Results[0].VehicleType,null,2)}> </ListItemText> */}
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Primary Fuel Type" secondary={JSON.stringify(data.Results[0].FuelTypePrimary,null,2)}> </ListItemText>
+                    {/* <ListItemText primary="Primary Fuel Type" secondary={JSON.stringify(data.Results[0].FuelTypePrimary,null,2)}> </ListItemText> */}
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Fuel Injection Type" secondary={JSON.stringify(data.Results[0].FuelInjectionType,null,2)}> </ListItemText>
+                    {/* <ListItemText primary="Fuel Injection Type" secondary={JSON.stringify(data.Results[0].FuelInjectionType,null,2)}> </ListItemText> */}
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Brake System Type" secondary={JSON.stringify(data.Results[0].BrakeSystemType,null,2)}> </ListItemText>
+                    {/* <ListItemText primary="Brake System Type" secondary={JSON.stringify(data.Results[0].BrakeSystemType,null,2)}> </ListItemText> */}
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Drive Type" secondary={JSON.stringify(data.Results[0].DriveType,null,2)}> </ListItemText>
+                    {/* <ListItemText primary="Drive Type" secondary={JSON.stringify(data.Results[0].DriveType,null,2)}> </ListItemText> */}
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Maximum Weight Rating" secondary={JSON.stringify(data.Results[0].GVWR,null,2)}> </ListItemText>
+                    {/* <ListItemText primary="Maximum Weight Rating" secondary={JSON.stringify(data.Results[0].GVWR,null,2)}> </ListItemText> */}
                 </ListItem>
                 
                 
